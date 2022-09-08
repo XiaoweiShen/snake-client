1590ee7b17fce29b125888e57f4395f4c6cfaa02
Wei <snhiui@gmail.com>
2022-09-08 01:33:43 +0000

    test

---
+++ input.js
@@ -0,0 +1,32 @@
+const { stdin } = require("process");
+
+//input.js
+let connection;
+
+const setupInput = function(conn){
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  stdin.on("data",handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+ // process.stdin.write(key);
+  
+  if (key === '\u0003') 
+    process.exit();
+  if (key === 'a')
+    connection.write("Move: left");
+  if (key === 'w')
+    connection.write("Move: up");
+  if (key === 'd')
+    connection.write("Move: right");
+  if (key === 's')
+    connection.write("Move: down");
+};
+
+module.exports = setupInput;
+
